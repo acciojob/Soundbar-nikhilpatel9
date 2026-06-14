693e1b7d9319de5cb2b73475a9676c338c17cdf2
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-06-14 14:26:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ const buttons = document.querySelectorAll('.btn');
+        const stopBtn = document.querySelector('.stop');
+        let audio;
+
+        buttons.forEach((button)=>{
+            button.addEventListener('click',()=>{
+                if(audio){
+                    audio.pause();
+                }
+                audio = new Audio(`https://github.com/acciojob/Soundbar-nikhilpatel9/blob/master/sounds/${button.innerText}.mp3?raw=true`);
+                audio.play();
+            })
+        })
+
+        stopBtn.addEventListener('click',()=>{
+            if(audio){
+                audio.pause();
+            }
+        })
